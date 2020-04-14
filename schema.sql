c6740d3e1da0bb195fe7dc20a2ff30e2c5bf0cee
Jay Honnold <jayhonnold@gmail.com>
2020-04-14 17:49:00 -0500

    Updated the schema

--- schema.sql
+++ schema.sql
@@ -16,29 +16,13 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: fndash; Type: SCHEMA; Schema: -; Owner: postgres
---
-
-CREATE SCHEMA fndash;
-
-
-ALTER SCHEMA fndash OWNER TO postgres;
-
---
--- Name: SCHEMA fndash; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON SCHEMA fndash IS 'standard public schema';
-
-
 SET default_tablespace = '';
 
 --
--- Name: game; Type: TABLE; Schema: fndash; Owner: postgres
+-- Name: game; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE fndash.game (
+CREATE TABLE public.game (
     id integer NOT NULL,
     kills integer,
     placement character varying,
@@ -47,13 +31,13 @@ CREATE TABLE fndash.game (
 );
 
 
-ALTER TABLE fndash.game OWNER TO postgres;
+ALTER TABLE public.game OWNER TO postgres;
 
 --
--- Name: game_id_seq; Type: SEQUENCE; Schema: fndash; Owner: postgres
+-- Name: game_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE fndash.game_id_seq
+CREATE SEQUENCE public.game_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -62,20 +46,20 @@ CREATE SEQUENCE fndash.game_id_seq
     CACHE 1;
 
 
-ALTER TABLE fndash.game_id_seq OWNER TO postgres;
+ALTER TABLE public.game_id_seq OWNER TO postgres;
 
 --
--- Name: game_id_seq; Type: SEQUENCE OWNED BY; Schema: fndash; Owner: postgres
+-- Name: game_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE fndash.game_id_seq OWNED BY fndash.game.id;
+ALTER SEQUENCE public.game_id_seq OWNED BY public.game.id;
 
 
 --
--- Name: input; Type: TABLE; Schema: fndash; Owner: postgres
+-- Name: input; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE fndash.input (
+CREATE TABLE public.input (
     id integer NOT NULL,
     user_id integer,
     input_type character varying,
@@ -83,13 +67,13 @@ CREATE TABLE fndash.input (
 );
 
 
-ALTER TABLE fndash.input OWNER TO postgres;
+ALTER TABLE public.input OWNER TO postgres;
 
 --
--- Name: input_id_seq; Type: SEQUENCE; Schema: fndash; Owner: postgres
+-- Name: input_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE fndash.input_id_seq
+CREATE SEQUENCE public.input_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -98,20 +82,20 @@ CREATE SEQUENCE fndash.input_id_seq
     CACHE 1;
 
 
-ALTER TABLE fndash.input_id_seq OWNER TO postgres;
+ALTER TABLE public.input_id_seq OWNER TO postgres;
 
 --
--- Name: input_id_seq; Type: SEQUENCE OWNED BY; Schema: fndash; Owner: postgres
+-- Name: input_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE fndash.input_id_seq OWNED BY fndash.input.id;
+ALTER SEQUENCE public.input_id_seq OWNED BY public.input.id;
 
 
 --
--- Name: stat; Type: TABLE; Schema: fndash; Owner: postgres
+-- Name: stat; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE fndash.stat (
+CREATE TABLE public.stat (
     id integer NOT NULL,
     name character varying,
     mode character varying,
@@ -127,13 +111,13 @@ CREATE TABLE fndash.stat (
 );
 
 
-ALTER TABLE fndash.stat OWNER TO postgres;
+ALTER TABLE public.stat OWNER TO postgres;
 
 --
--- Name: stat_history; Type: TABLE; Schema: fndash; Owner: postgres
+-- Name: stat_history; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE fndash.stat_history (
+CREATE TABLE public.stat_history (
     id integer NOT NULL,
     stat_id integer,
     placements jsonb,
@@ -145,13 +129,13 @@ CREATE TABLE fndash.stat_history (
 );
 
 
-ALTER TABLE fndash.stat_history OWNER TO postgres;
+ALTER TABLE public.stat_history OWNER TO postgres;
 
 --
--- Name: stat_history_id_seq; Type: SEQUENCE; Schema: fndash; Owner: postgres
+-- Name: stat_history_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE fndash.stat_history_id_seq
+CREATE SEQUENCE public.stat_history_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -160,20 +144,20 @@ CREATE SEQUENCE fndash.stat_history_id_seq
     CACHE 1;
 
 
-ALTER TABLE fndash.stat_history_id_seq OWNER TO postgres;
+ALTER TABLE public.stat_history_id_seq OWNER TO postgres;
 
 --
--- Name: stat_history_id_seq; Type: SEQUENCE OWNED BY; Schema: fndash; Owner: postgres
+-- Name: stat_history_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE fndash.stat_history_id_seq OWNED BY fndash.stat_history.id;
+ALTER SEQUENCE public.stat_history_id_seq OWNED BY public.stat_history.id;
 
 
 --
--- Name: stat_id_seq; Type: SEQUENCE; Schema: fndash; Owner: postgres
+-- Name: stat_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE fndash.stat_id_seq
+CREATE SEQUENCE public.stat_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -182,20 +166,48 @@ CREATE SEQUENCE fndash.stat_id_seq
     CACHE 1;
 
 
-ALTER TABLE fndash.stat_id_seq OWNER TO postgres;
+ALTER TABLE public.stat_id_seq OWNER TO postgres;
 
 --
--- Name: stat_id_seq; Type: SEQUENCE OWNED BY; Schema: fndash; Owner: postgres
+-- Name: stat_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE fndash.stat_id_seq OWNED BY fndash.stat.id;
+ALTER SEQUENCE public.stat_id_seq OWNED BY public.stat.id;
 
 
 --
--- Name: user; Type: TABLE; Schema: fndash; Owner: postgres
+-- Name: stat_placement_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE fndash."user" (
+CREATE SEQUENCE public.stat_placement_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.stat_placement_id_seq OWNER TO postgres;
+
+--
+-- Name: stat_placement; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.stat_placement (
+    id integer DEFAULT nextval('public.stat_placement_id_seq'::regclass) NOT NULL,
+    stat_id integer,
+    placement character varying(8),
+    count integer
+);
+
+
+ALTER TABLE public.stat_placement OWNER TO postgres;
+
+--
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."user" (
     id integer NOT NULL,
     uid character varying(32) NOT NULL,
     username character varying(255),
@@ -205,13 +217,13 @@ CREATE TABLE fndash."user" (
 );
 
 
-ALTER TABLE fndash."user" OWNER TO postgres;
+ALTER TABLE public."user" OWNER TO postgres;
 
 --
--- Name: user_id_seq; Type: SEQUENCE; Schema: fndash; Owner: postgres
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE fndash.user_id_seq
+CREATE SEQUENCE public.user_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -220,143 +232,159 @@ CREATE SEQUENCE fndash.user_id_seq
     CACHE 1;
 
 
-ALTER TABLE fndash.user_id_seq OWNER TO postgres;
+ALTER TABLE public.user_id_seq OWNER TO postgres;
 
 --
--- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: fndash; Owner: postgres
+-- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE fndash.user_id_seq OWNED BY fndash."user".id;
-
-
---
--- Name: game id; Type: DEFAULT; Schema: fndash; Owner: postgres
---
-
-ALTER TABLE ONLY fndash.game ALTER COLUMN id SET DEFAULT nextval('fndash.game_id_seq'::regclass);
+ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
 
 
 --
--- Name: input id; Type: DEFAULT; Schema: fndash; Owner: postgres
+-- Name: game id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.input ALTER COLUMN id SET DEFAULT nextval('fndash.input_id_seq'::regclass);
-
-
---
--- Name: stat id; Type: DEFAULT; Schema: fndash; Owner: postgres
---
-
-ALTER TABLE ONLY fndash.stat ALTER COLUMN id SET DEFAULT nextval('fndash.stat_id_seq'::regclass);
+ALTER TABLE ONLY public.game ALTER COLUMN id SET DEFAULT nextval('public.game_id_seq'::regclass);
 
 
 --
--- Name: stat_history id; Type: DEFAULT; Schema: fndash; Owner: postgres
+-- Name: input id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.stat_history ALTER COLUMN id SET DEFAULT nextval('fndash.stat_history_id_seq'::regclass);
-
-
---
--- Name: user id; Type: DEFAULT; Schema: fndash; Owner: postgres
---
-
-ALTER TABLE ONLY fndash."user" ALTER COLUMN id SET DEFAULT nextval('fndash.user_id_seq'::regclass);
+ALTER TABLE ONLY public.input ALTER COLUMN id SET DEFAULT nextval('public.input_id_seq'::regclass);
 
 
 --
--- Name: stat _input_name_mode_uc; Type: CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: stat id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.stat
+ALTER TABLE ONLY public.stat ALTER COLUMN id SET DEFAULT nextval('public.stat_id_seq'::regclass);
+
+
+--
+-- Name: stat_history id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.stat_history ALTER COLUMN id SET DEFAULT nextval('public.stat_history_id_seq'::regclass);
+
+
+--
+-- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+
+
+--
+-- Name: stat _input_name_mode_uc; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.stat
     ADD CONSTRAINT _input_name_mode_uc UNIQUE (input_id, name, mode);
 
 
 --
--- Name: game game_pkey; Type: CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: game game_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.game
+ALTER TABLE ONLY public.game
     ADD CONSTRAINT game_pkey PRIMARY KEY (id);
 
 
 --
--- Name: input input_pkey; Type: CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: input input_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.input
+ALTER TABLE ONLY public.input
     ADD CONSTRAINT input_pkey PRIMARY KEY (id);
 
 
 --
--- Name: stat_history stat_history_pkey; Type: CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: stat_history stat_history_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.stat_history
+ALTER TABLE ONLY public.stat_history
     ADD CONSTRAINT stat_history_pkey PRIMARY KEY (id);
 
 
 --
--- Name: stat stat_pkey; Type: CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: stat stat_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.stat
+ALTER TABLE ONLY public.stat
     ADD CONSTRAINT stat_pkey PRIMARY KEY (id);
 
 
 --
--- Name: user user_pkey; Type: CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: stat_placement stat_placement_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash."user"
+ALTER TABLE ONLY public.stat_placement
+    ADD CONSTRAINT stat_placement_pk PRIMARY KEY (id);
+
+
+--
+-- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 
 
 --
--- Name: user user_uid_key; Type: CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: user user_uid_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash."user"
+ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_uid_key UNIQUE (uid);
 
 
 --
--- Name: _name_mode_ix; Type: INDEX; Schema: fndash; Owner: postgres
+-- Name: _name_mode_ix; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE INDEX _name_mode_ix ON fndash.stat USING btree (name, mode);
-
-
---
--- Name: game game_stat_id_fkey; Type: FK CONSTRAINT; Schema: fndash; Owner: postgres
---
-
-ALTER TABLE ONLY fndash.game
-    ADD CONSTRAINT game_stat_id_fkey FOREIGN KEY (stat_id) REFERENCES fndash.stat(id);
+CREATE INDEX _name_mode_ix ON public.stat USING btree (name, mode);
 
 
 --
--- Name: input input_user_id_fkey; Type: FK CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: game game_stat_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.input
-    ADD CONSTRAINT input_user_id_fkey FOREIGN KEY (user_id) REFERENCES fndash."user"(id);
-
-
---
--- Name: stat_history stat_history_stat_id_fkey; Type: FK CONSTRAINT; Schema: fndash; Owner: postgres
---
-
-ALTER TABLE ONLY fndash.stat_history
-    ADD CONSTRAINT stat_history_stat_id_fkey FOREIGN KEY (stat_id) REFERENCES fndash.stat(id);
+ALTER TABLE ONLY public.game
+    ADD CONSTRAINT game_stat_id_fkey FOREIGN KEY (stat_id) REFERENCES public.stat(id);
 
 
 --
--- Name: stat stat_input_id_fkey; Type: FK CONSTRAINT; Schema: fndash; Owner: postgres
+-- Name: input input_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY fndash.stat
-    ADD CONSTRAINT stat_input_id_fkey FOREIGN KEY (input_id) REFERENCES fndash.input(id);
+ALTER TABLE ONLY public.input
+    ADD CONSTRAINT input_user_id_fkey FOREIGN KEY (user_id) REFERENCES public."user"(id);
+
+
+--
+-- Name: stat_history stat_history_stat_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.stat_history
+    ADD CONSTRAINT stat_history_stat_id_fkey FOREIGN KEY (stat_id) REFERENCES public.stat(id);
+
+
+--
+-- Name: stat stat_input_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.stat
+    ADD CONSTRAINT stat_input_id_fkey FOREIGN KEY (input_id) REFERENCES public.input(id);
+
+
+--
+-- Name: stat_placement stat_placement_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.stat_placement
+    ADD CONSTRAINT stat_placement_fk FOREIGN KEY (stat_id) REFERENCES public.stat(id);
 
 
 --
